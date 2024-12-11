c07d049e7a6068c1d4c1517d7b74ed60d8e4f32f
MasonFields24 <fieldsmn@miamioh.edu>
2024-12-10 21:34:32 -0500

    Added health system and UI
    Enemies now deal damage. There is a death animation. Also added pause feature

--- Source/cse389FinalProject/SkeletonEnemy.cpp
+++ Source/cse389FinalProject/SkeletonEnemy.cpp
@@ -2,4 +2,39 @@
 
 
 #include "SkeletonEnemy.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "PaperTileMapComponent.h"
+#include "Ninja.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+ASkeletonEnemy::ASkeletonEnemy()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	bIsDead = false;
+}
+
+void ASkeletonEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ASkeletonEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+bool ASkeletonEnemy::GetIsDead()
+{
+	return bIsDead;
+}
+
+void ASkeletonEnemy::SetIsDead(bool b)
+{
+	bIsDead = b;
+}
 
